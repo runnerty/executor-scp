744d9def30c532f86f6c9fe76715a83e8b083343
carlos <carloscarmonaalcantara@gmail.com>
2017-04-28 11:35:58 +0200

    Simplify executor development

--- index.js
+++ index.js
@@ -9,42 +9,30 @@ class scpExecutor extends Execution {
     super(process);
   }
 
-  exec() {
+  exec(params) {
     var _this = this;
-    var endOptions = {end: 'end'};
+    var endOptions = {};
 
-    return new Promise(function (resolve, reject) {
-      _this.getValues()
-        .then((res) => {
-          var scpCommand = `scp ${(res.identityFile) ? '-i' : ''} ${res.identityFile} ${res.localFile} ${res.remoteUser}@${res.remoteHost}:${res.remoteFilePath}`;
-          endOptions.command_executed = scpCommand;
-          var proc = spawn(scpCommand, [], {shell: true});
+    var scpCommand = `scp ${(params.identityFile) ? '-i' : ''} ${params.identityFile} ${params.localFile} ${params.remoteUser}@${params.remoteHost}:${params.remoteFilePath}`;
+    endOptions.command_executed = scpCommand;
+    var proc = spawn(scpCommand, [], {shell: true});
 
-          var stderr = '';
-          proc.stderr.on('data', function (chunk) {
-            stderr += chunk;
-          });
-
-          proc
-            .on('close', function (code, signal) {
-              if (code) {
-                endOptions.end = 'error';
-                endOptions.messageLog = `SCP Error (${scpCommand}): ${signal} / ${stderr}`;
-                endOptions.execute_err_return = `SCP Error (${scpCommand}): ${signal} / ${stderr}`;
-                _this.end(endOptions, resolve, reject);
-              } else {
-                endOptions.end = 'end';
-                _this.end(endOptions, resolve, reject);
-              }
-            });
-        })
-        .catch((err) => {
-          endOptions.end = 'error';
-          endOptions.messageLog = `SCP Error getValues: ${err}`;
-          endOptions.execute_err_return = `SCP Error getValues: ${err}`;
-          _this.end(endOptions, resolve, reject);
-        });
+    var stderr = '';
+    proc.stderr.on('data', function (chunk) {
+      stderr += chunk;
     });
+
+    proc
+      .on('close', function (code, signal) {
+        if (code) {
+          endOptions.end = 'error';
+          endOptions.messageLog = `SCP Error (${scpCommand}): ${signal} / ${stderr}`;
+          endOptions.execute_err_return = `SCP Error (${scpCommand}): ${signal} / ${stderr}`;
+          _this.end(endOptions);
+        } else {
+          _this.end();
+        }
+      });
   }
 }
 
